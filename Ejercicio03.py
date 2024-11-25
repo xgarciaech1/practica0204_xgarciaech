704a6e28a5d2b0833021203300444f38509e4a27
xgarciaech1 <xgarciaech@educacion.navarra.es>
2024-11-25 10:02:57 +0100

    Update Ejercicio03.py

--- Ejercicio03.py
+++ Ejercicio03.py
@@ -2,13 +2,12 @@
 #artículos de la tabla, pregunte al usuario por un artículo, un número de
 #unidades y muestre por pantalla el precio de esa cantidad de producto. Si el
 #producto no está en el diccionario debe mostrar un mensaje informando de ello
-cesta = {'Pan' : '1.4', 'huevos' :  '2.3', 'cebolla' : '0.85',
-          'aceite' : '4.35'}
+cesta = {'Pan':1.4, 'huevos':2.3, 'cebolla':0.85,
+          'aceite':4.35}
 articulo = input('Dime un articulo: ').capitalize()
 cantidad = int(input('Dime la cantidad que quires: '))
 if articulo in cesta:
     Cesta = (cesta.get(articulo))
-    print(Cesta)
     total = ((Cesta) * (cantidad))
     print(total)
 else:
