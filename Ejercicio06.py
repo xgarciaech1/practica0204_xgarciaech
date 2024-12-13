3ff3e9c4f016f32fb3d65d8220ebe7330fb75d37
xgarciaech1 <xgarciaech@educacion.navarra.es>
2024-12-13 08:58:55 +0100

    Update Ejercicio06.py

--- Ejercicio06.py
+++ Ejercicio06.py
@@ -9,12 +9,14 @@ while opcion != '6':
         aprobado = input('¿Es un aprobado? (S/N)? ').upper()
         alumno = {'nombre':nombre, 'teléfono':telefono, 'email':email, 'aprobado':aprobado =='S'}
         diccionario[nif] = alumno
+        
     if opcion == '2':
         nif = input('Introduce NIF del alumno: ')
         if nif in diccionario:
             del diccionario[nif]
         else:
             print('No existe el alumno con el nif', nif)
+
     if opcion == '3':
         nif = input('Introduce NIF del alumno: ')
         if nif in diccionario:
@@ -22,14 +24,15 @@ while opcion != '6':
                 print(clave.title() + ':', valor)
         else:
             print('No existe el alumno con el nif', nif)
+
     if opcion == '4':
         print('Lista de alumno')
         for clave, valor in diccionario.items():
             print(clave, valor['nombre'])
+
     if opcion == '5':
         print('Lista de alumnos aprobados')
         if aprobado=='S':
             print(nombre)
-    
-   
+
     opcion = input('Menú de opciones\n(1) Añadir alumno\n(2) Eliminar alumno\n(3) Mostrar alumno\n(4) Listar alumno\n(5) Listar alumnos aprovados\n(6) Terminar\nElige una opción:')
